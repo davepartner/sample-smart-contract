ecad3261b1e2f864023e56acebc74af4e1357dff
Dave <daveozoalor@gmail.com>
2021-11-30 23:26:46 +0100

    Update Founders.sol

--- Founders.sol
+++ Founders.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8; 
+pragma solidity ^0.8;
 
-/// @notice a smart contract to vest the tokens of founders for a period of time before they can withdraw
 contract Vesting{
-
 
     struct Founder {
         uint256 amount;
@@ -14,31 +12,37 @@ contract Vesting{
     //@notice things we have to save
     mapping(address => Founder) public founders;
     address public admin;
+    uint256 public transactionValue;
 
-     // event Transfer(address indexed from, address indexed to, uint256 value);
-     // event Approval(address indexed owner, address indexed spender, uint256 value);
+    event addFounderEvent(address _founderEvent, uint256 _timeToMaturityEvent, string _messageEvent);
 
-    constructor(){
+    constructor() payable{
         admin = msg.sender;
+        transactionValue = msg.value;
     }
 
     /// @notice add new founder
+    /// @param _founder: address of the founder to be added
+    /// @param _timeToMaturity : integer of time
     function addFounder(address _founder, uint256 _timeToMaturity) external payable{
         require(_founder == admin, 'only admin allowed'); //check if spender is admin
         require(founders[_founder].amount == 0, 'founder already exists'); // check if founder already exists
 
-        founders[_founder] = Founder(msg.value, block.timestamp + _timeToMaturity, false);
+        founders[_founder] = Founder(transactionValue, block.timestamp + _timeToMaturity, false);
 
+        emit addFounderEvent(_founder, transactionValue, 'addFounder event message');
     }
 
-    function withdraw() external returns (bool) {
+    /// @notice withdrawal function
+    function withdraw() external payable{
         Founder storage founder = founders[msg.sender];
         require(founder.maturity <= block.timestamp, 'too early'); //the date for withdrawal must have passed
         require(founder.amount > 0, 'only admin can withdraw' );
         require(founder.paid == false, 'paid already');
         founder.paid = true;
         payable(msg.sender).transfer(founder.amount);
-        return true;
+
+        emit addFounderEvent(msg.sender, founder.amount, 'withdrawal event message');
     }
 
 }
